fa72e093563dfab29a2e6f8450aa72a085e80ddd
AJ Seidler <ajseidler0526@gmail.com>
2023-10-27 18:51:53 -0500

    finished model files

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -13,7 +13,7 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    product_tag: {
+    product_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
