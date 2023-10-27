77cb2e1469135f680d186f8dc1ddc1faf1c7d39a
AJ Seidler <ajseidler0526@gmail.com>
2023-10-27 18:34:11 -0500

    worked on functionality for models

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,77 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const locationData = Location.findAll();
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const locationData = Location.findByPk(req.params.id, {
+      include: [{ model: Traveller, through: Trip, as: 'trips_by_traveller' }]
+    });
+    if (!locationData) {
+      res.status(404).json({ message: 'No location with this id!' });
+      return;
+    }
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const locationData = Location.create(req.body);
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const locationData = Location.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!locationData[0]) {
+      res.status(404).json({ message: 'No location with this id!' });
+      return;
+    }
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const locationData = Location.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!locationData) {
+      res.status(404).json({ message: 'No location found with that id!' });
+      return;
+    }
+
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+  app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}`));
 });
